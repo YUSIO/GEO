3aabf9373cea518122b9db42be06e79e41791b63
YUSIO <wjz54250@outlook.com>
2021-07-09 00:02:31 +0800

    增加了修饰性图片

--- GEO/resource.h
+++ GEO/resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_GEO_DIALOG                  102
 #define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     131
 #define IDC_EDIT_La1                    1000
 #define IDC_EDIT_Lo1                    1001
 #define IDC_EDIT_La2                    1002
@@ -21,7 +22,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
